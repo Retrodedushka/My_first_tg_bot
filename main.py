dd101d4ff4acad0f269b9f7ec392e264f37d22fc
Retrodedushka <bondglib@gmail.com>
2026-04-20 22:19:21 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram.types import Message
+
+bot = Bot(token='8717686709:AAFTVHUwdqG1FCSwxermCpVY_Fv6np-bQWM')
+dp = Dispatcher()
+
+@dp.message()
+async def start_handler(message: Message): 
+    await message.answer("Привет! Я готов показать дивиденды.")
+
+async def main():
+    print("Ожидаю сообщения...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
