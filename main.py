d9b751a920dc8c5cd16794cd485f7134f94382a0
Retrodedushka <bondglib@gmail.com>
2026-04-21 19:28:48 +0700

    Убрал лишних функций, добавил общий словарь

--- main.py
+++ main.py
@@ -7,31 +7,24 @@ from aiogram import F
 session = AiohttpSession()
 bot = Bot(token='8717686709:AAFTVHUwdqG1FCSwxermCpVY_Fv6np-bQWM', session=session)
 dp = Dispatcher()
+companies = {
+    "Лукойл": "06 мая 2026 года",
+    "Сбер": "15 июля 2025 года",
+    "Озон": "28 мая 2025 года",
+    "Яндекс": "13 апреля 2025 года"
+}
 
 @dp.message(F.text == "/start")
 async def cmd_start(message: Message):
-    buttons = [
-        [KeyboardButton(text="Лукойл"), KeyboardButton(text="Сбер")],
-        [KeyboardButton(text="Яндекс"), KeyboardButton(text="Озон")]
-    ]
+    buttons = [[KeyboardButton(text=name)] for name in companies.keys()]
     kb = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
     await message.answer("Привет! Выбери компанию:", reply_markup=kb)
 
-@dp.message(F.text == "Лукойл")
-async def lukoil_handler(message: Message):
-    await message.answer("Ближайшие дивиденды Лукойл: 15 мая 2025 года.")
-
-@dp.message(F.text == "Сбер")
-async def sber_handler(message: Message):
-    await message.answer("Ближайшие дивиденды Сбер: июль 2025 (рекомендованны).")
-
-@dp.message(F.text == "Озон")
-async def ozon_handler(message: Message):
-    await message.answer("Ближайшие дивиденды Озон: май 2025 (рекомендованны).")
-
-@dp.message(F.text == "Яндекс")
-async def yandex_handler(message: Message):
-    await message.answer("Ближайшие дивиденды Яндекс: апрель 2025 (рекомендованны).")
+@dp.message(F.text.in_(companies.keys()))
+async def company_info(message: Message):
+    name = message.text
+    date = companies.get(name)
+    await message.answer(f"Дата публикации финансовой отчетности для {name}: {date}")
 
 async def main():
     print("Ожидаю сообщения...")
